c7f990c8f2f25cfe3a51a1d343330c4dbb2c3c7f
adfsgherw1 <123621351+adfsgherw1@users.noreply.github.com>
2023-11-27 22:57:55 -0600

    Update dqn.py

--- rl/agents/dqn.py
+++ rl/agents/dqn.py
@@ -65,6 +65,7 @@ class AbstractDQNAgent(Agent):
         return q_values
 
     def compute_q_values(self, state):
+        print(state)
         q_values = self.compute_batch_q_values([state]).flatten()
         assert q_values.shape == (self.nb_actions,)
         return q_values
